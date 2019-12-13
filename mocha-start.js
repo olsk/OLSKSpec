5088bf897fd137bb62a473ae58ef9ff6b9acb5a1
Rosano Coutinho <pub@xeari.com>
2019-12-13 11:29:51 -0500

    Extract _ControlExtendZombie

--- mocha-start.js
+++ mocha-start.js
@@ -1,18 +1,206 @@
-(function OLSKMochaBrowser() {
-	if (process.env.OLSK_TESTING_BEHAVIOUR !== 'true') {
-		return;
-	}
+const mod = {
 
-	const Browser = require('zombie');
+	// CONTROL
 
-	Browser.localhost('loc.tests', process.env.PORT || 3000);
-	
-	require('OLSKTesting')._OLSKTestingZombieExtend(Browser);
+	_ControlExtendZombie(Browser) {
+		Browser.prototype.OLSKFireKeyboardEvent = function(target, keyCode, eventOptions = {}) {
+			const event = this.window.document.createEvent('HTMLEvents');
+			event.initEvent('keydown', true, true);
+			event.which = event.keyCode = event.key = event.code = keyCode;
 
-	global.OLSKBrowser = Browser;
+			target = typeof target === 'string' ? this.query(target) : target;
 
-	global.browser = new OLSKBrowser();
-})();
+			if (!target) {
+				throw new Error('no target');
+			}
+
+			target.dispatchEvent(Object.assign(event, eventOptions));
+
+			return this._wait(null);
+		};
+
+		Browser.extend(function(browser) {
+		  browser.on('confirm', function(confirm) {
+		    return browser.OLSKConfirmCallback ? browser.OLSKConfirmCallback(confirm) : confirm;
+		  });
+		});
+		
+		Browser.prototype.OLSKConfirmSync = function(inputData) {
+			let outputData = undefined;
+
+			let browser = this;
+			browser.OLSKConfirmCallback = function (confirm) {
+				delete browser.OLSKConfirmCallback;
+
+				outputData = confirm;
+			};
+
+			inputData();
+
+			return outputData;
+		};
+		
+		Browser.prototype.OLSKConfirm = async function(param1, param2) {
+			let browser = this;
+			return await new Promise(async function (resolve, reject) {
+				browser.OLSKConfirmCallback = function (confirm) {
+					delete browser.OLSKConfirmCallback;
+
+					return resolve(param2 ? param2(confirm) : confirm);
+				};
+
+				param1();
+			});
+		};
+
+		Browser.extend(function(browser) {
+		  browser.on('alert', function(alert) {
+		    return browser.OLSKAlertCallback ? browser.OLSKAlertCallback(alert) : alert;
+		  });
+		});
+		
+		Browser.prototype.OLSKAlert = function(inputData) {
+			let outputData = undefined;
+
+			let browser = this;
+			browser.OLSKAlertCallback = function (alert) {
+				delete browser.OLSKAlertCallback;
+
+				outputData = alert;
+			};
+
+			inputData();
+
+			return outputData;
+		};
+
+		Browser.extend(function(browser) {
+		  browser.on('OLSKMessage', function(data) {
+		    return browser.OLSKMessageCallback ? browser.OLSKMessageCallback(data) : data;
+		  });
+		});
+		
+		Browser.prototype.OLSKMessage = function(inputData) {
+			let outputData = undefined;
+
+			let browser = this;
+			browser.OLSKMessageCallback = function (data) {
+				delete browser.OLSKMessageCallback;
+
+				outputData = data;
+			};
+
+			inputData();
+
+			return outputData;
+		};
+		
+		Browser.prototype.OLSKMessageAsync = function(inputData) {
+			let outputData = undefined;
+
+			let browser = this;
+			browser.OLSKMessageCallback = function (data) {
+				delete browser.OLSKMessageCallback;
+
+				outputData = data;
+			};
+
+			return new Promise(async function (resolve, reject) {
+				await inputData();
+
+				return resolve(outputData)
+			});
+		};
+
+		Browser.extend(function(browser) {
+		  browser.on('request', function(request) {
+		    return browser.OLSKRequestCallback ? browser.OLSKRequestCallback(request) : request;
+		  });
+		});
+		
+		Browser.prototype.OLSKRequest = function(inputData) {
+			let outputData = undefined;
+
+			let browser = this;
+			browser.OLSKRequestCallback = function (request) {
+				delete browser.OLSKRequestCallback;
+
+				outputData = request;
+			};
+
+			inputData();
+
+			return outputData;
+		};
+
+		Browser.extend(function(browser) {
+		  browser.on('response', function(request, response) {
+		    return browser.OLSKResponseCallback ? browser.OLSKResponseCallback(response) : response;
+		  });
+		});
+		
+		Browser.prototype.OLSKResponse = function(inputData) {
+			let browser = this;
+			
+			return new Promise(async function (resolve, reject) {
+				browser.OLSKResponseCallback = function (outputData) {
+					delete browser.OLSKResponseCallback;
+
+					return resolve(outputData)
+				};
+
+				inputData();
+			});
+		};
+		
+		Browser.prototype.OLSKVisit = function(routeObject, params) {
+			return this.visit(global.OLSKTestingCanonicalFor(routeObject.OLSKRoutePath, params))
+		};
+
+		Browser.extend(function(browser) {
+		  browser.pipeline.addHandler(function(browser, request, response) {
+		  	if (request.url.match('player.vimeo.com')) {
+		  		return Object.assign(response, {
+		  			status: 200,
+		  		})
+		  	}
+		    
+		    return response;
+		  });
+		});
+	},
+
+	// SETUP
+
+	SetupEverything() {
+		mod.SetupZombie();
+	},
+
+	SetupZombie() {
+		if (process.env.OLSK_TESTING_BEHAVIOUR !== 'true') {
+			return;
+		}
+
+		const Browser = require('zombie');
+
+		Browser.localhost('loc.tests', process.env.PORT || 3000);
+		
+		mod._ControlExtendZombie(Browser);
+
+		global.OLSKBrowser = Browser;
+
+		global.browser = new OLSKBrowser();
+	},
+
+	// LIFECYCLE
+
+	LifecycleModuleDidLoad() {
+		mod.SetupEverything();
+	},
+
+};
+
+mod.LifecycleModuleDidLoad();
 
 let languageDictionary = {};
 (function OLSKMochaLocalizedStrings() {
